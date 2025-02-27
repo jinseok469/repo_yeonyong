a43df683db2712a1ce0504ce68cc66c2ebe4a1a4
jinseok469 <jinseok469@naver.com>
2025-02-27 12:05:16 +0900

    0227

---
+++ yeonyong/src/test/java/com/mickey/demo/YeonyongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mickey.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeonyongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
